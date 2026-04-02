49ee74e7fbb78b1e0e24d67ecd73f06e43688ae2
WoutBoonenPXL <wout.boonen@student.pxl.be>
2026-04-02 09:39:20 +0200

    fix: use CommonJS exports in package entrypoint

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-export * from "./Frontend/Pages/Components/Navbar";
-export * from "./Frontend/Pages/Components/Footer";
-export * from "./Frontend/Pages/Components/Form";
-
-export * from "./Frontend/Pages/BasePage";
-export * from "./Frontend/Pages/HomePage";
-export * from "./Frontend/Pages/AboutPage";
-export * from "./Frontend/Pages/ContactPage";
-export * from "./Frontend/Pages/LoginPage";
-export * from "./Frontend/Pages/CartPage";
-export * from "./Frontend/Pages/NotFoundPage";
-export * from "./Frontend/Pages/AdminPage";
-
 module.exports = {
   ...require('./API/SOM/backend-api.som'),
   ...require('./API/SOM/products-api.som'),
